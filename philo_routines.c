1c5bb304af16201c06d1b9a8ad3ed4edff24772c
Serhii Nazarov <mypmypmew.dev@gmail.com>
2025-01-23 22:13:39 +0100

    starting with philo routine, main algo + 2 utils functions (print + sleep)

--- philo_routines.c
+++ philo_routines.c
@@ -1,23 +1,50 @@
 #include "philo.h"
 
-void philo_eats()
-{
 
+void action_print(t_info *rules, int philo_id, const char *msg)
+{
+	long now;
+
+	pthread_mutex_lock(&rules->print_mutex);
+
+	if (!rules->finished)
+	{
+		now = get_current_time_ms() - rules->start_time;
+		printf("%ld %d %s\n", now, philo_id, msg);
+	}
+
+	pthread_mutex_unlock(&rules->print_mutex);
 }
 
-void philo_sleep()
+void smart_sleep(int duration_ms, t_info *rules)
 {
-
+	long start = get_current_time_ms();
+	while (!rules->finished && (get_current_time_ms() - start < duration_ms))
+		usleep(100);  // Sleep 100 microseconds at a time
 }
 
-/*
-берем вилки мин - макс
-кладем - большую потом меньшую (ид)
-задержка
-*/
-
-
-void routine()
+void *philo_routine(void *arg)
 {
+	t_philosophers *philo = (t_philosophers *)arg;
+	t_info         *rules = philo->info;
 
+	if (philo->philosophers_id % 2 == 0)
+		usleep(15000);
+
+	while (!rules->finished)
+	{
+		philo_eats(philo);
+
+		if (rules->finished)
+			break;
+
+		action_print(rules, philo->philosophers_id, "is sleeping");
+		smart_sleep(rules->time_to_sleep, rules);
+		if (rules->finished)
+			break;
+
+		action_print(rules, philo->philosophers_id, "is thinking");
+	}
+
+	return (NULL);
 }
