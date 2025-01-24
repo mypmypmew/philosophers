a10dc3fc91c140bd3125a0dd32cecde433d98ea8
Serhii Nazarov <mypmypmew.dev@gmail.com>
2025-01-24 18:43:28 +0100

    added launcher for simulation

--- simulation.c
+++ simulation.c
@@ -1,46 +1,38 @@
 
 #include "philo.h"
 
-void *debug_routine(void *arg)
+
+int launcher(t_info *rules)
 {
-	t_philosophers *philo = (t_philosophers *)arg;
+	int i;
 
-	pthread_mutex_lock(&philo->info->print_mutex);
-	printf("[DEBUG] Hello from philosopher %d\n", philo->philosophers_id);
-	printf("[DEBUG]  -> left fork id:  %d\n", philo->left_fork->id);
-	printf("[DEBUG]  -> right fork id: %d\n", philo->right_fork->id);
-	printf("[DEBUG]  -> time_to_die:   %d\n", philo->info->time_to_die);
-	printf("[DEBUG]  -> time_to_eat:   %d\n", philo->info->time_to_eat);
-	printf("[DEBUG]  -> time_to_sleep: %d\n", philo->info->time_to_sleep);
-	pthread_mutex_unlock(&philo->info->print_mutex);
-	usleep(1000 * 1000);
-	return (NULL);
-}
+	rules->start_time = get_current_time_ms();
 
-int start_simulation_debug(t_info *rules)
-{
-	int i = 0;
-	t_philosophers *philos = rules->philosophers;
-
-	while (i < rules->philosophers_number)
-	{
-		if (pthread_create(
-				&philos[i].thread_id,
-				NULL,
-				debug_routine,
-				&philos[i]
-			) != 0)
-		{
-			printf("Error: pthread_create failed for philosopher %d\n", i + 1);
-			return (0);
-		}
-		i++;
-	}
 	i = 0;
 	while (i < rules->philosophers_number)
 	{
-		pthread_join(philos[i].thread_id, NULL);
+		rules->philosophers[i].last_meal = rules->start_time;
+
+		if (pthread_create(
+			&rules->philosophers[i].thread_id,
+			NULL,
+			philo_routine,
+			&rules->philosophers[i]) != 0)
+		{
+			printf("Error creating thread %d\n", i);
+			return (1);
+		}
 		i++;
 	}
-	return (1);
+
+	death_checker(rules);
+
+	i = 0;
+	while (i < rules->philosophers_number)
+	{
+		pthread_join(rules->philosophers[i].thread_id, NULL);
+		i++;
+	}
+
+	return (0);
 }
