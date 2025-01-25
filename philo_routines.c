f3759e19ed8945bb932834bee79a2ce5d8943255
Serhii Nazarov <mypmypmew.dev@gmail.com>
2025-01-25 22:16:59 +0100

    fully refactored (25 lines) and double checked with hellgrind

--- philo_routines.c
+++ philo_routines.c
@@ -6,16 +6,15 @@
 void action_print(t_info *rules, int philo_id, const char *msg)
 {
 	long now;
+
 	pthread_mutex_lock(&rules->dead_lock);
 	if (!rules->finished)
 	{
 		pthread_mutex_unlock(&rules->dead_lock);
-
 		pthread_mutex_lock(&rules->print_mutex);
 
 		now = get_current_time_ms() - rules->start_time;
 		printf("%ld %d %s\n", now, philo_id, msg);
-
 		pthread_mutex_unlock(&rules->print_mutex);
 	}
 	else
@@ -31,82 +30,81 @@ static long long current_time_microseconds(void)
 	return ((long long)now.tv_sec * 1000000) + now.tv_usec;
 }
 
+void check_finished(t_philosophers *philo, int *local_finished)
+{
+	pthread_mutex_lock(&philo->info->dead_lock);
+	*local_finished = philo->info->finished;
+	pthread_mutex_unlock(&philo->info->dead_lock);
+}
+
+long long	calculate_remaining_time(long long start, long long target_us)
+{
+	long long now;
+	now = current_time_microseconds();
+	return (target_us - (now - start));
+}
+
+void	short_sleep_loop(long long start, long long target_us, t_philosophers *philo)
+{
+	int local_finished;
+
+	while (1)
+	{
+		check_finished(philo, &local_finished);
+		if (local_finished)
+			break;
+		if (current_time_microseconds() - start >= target_us)
+			break;
+	}
+}
+
 void philo_usleep(long long target_us, t_philosophers *philo)
 {
 	long long start;
-	long long now;
-	long long elapsed;
+	int local_finished;
 	long long remaining;
 
 	start = current_time_microseconds();
 	while (1)
 	{
-		// 1) Check if simulation finished
-		pthread_mutex_lock(&philo->info->dead_lock);
-		int local_finished = philo->info->finished;
-		pthread_mutex_unlock(&philo->info->dead_lock);
-
+		check_finished(philo, &local_finished);
 		if (local_finished)
 			break;
-
-		// 2) Check how long we've slept so far
-		now = current_time_microseconds();
-		elapsed = now - start;
-		if (elapsed >= target_us)
+		remaining = calculate_remaining_time(start, target_us);
+		if (remaining <= 0)
 			break;
-		remaining = target_us - elapsed;
 		if (remaining > 1000)
 			usleep(remaining / 2);
 		else
 		{
-			while (1)
-			{
-				pthread_mutex_lock(&philo->info->dead_lock);
-				local_finished = philo->info->finished;
-				pthread_mutex_unlock(&philo->info->dead_lock);
-				if (local_finished)
-					break;
-
-				now = current_time_microseconds();
-				if ((now - start) >= target_us)
-					break;
-			}
+			short_sleep_loop(start, target_us, philo);
 			break;
 		}
 	}
 }
 
-void philo_eats(t_philosophers *philo)
+void lock_forks(t_philosophers *philo)
 {
-	t_info *rules = philo->info;
-
 	if (philo->left_fork->id < philo->right_fork->id)
 	{
 		pthread_mutex_lock(&philo->left_fork->fork);
-		action_print(rules, philo->philosophers_id, "has taken a fork");
+		action_print(philo->info, philo->philosophers_id, "has taken a fork");
 
 		pthread_mutex_lock(&philo->right_fork->fork);
-		action_print(rules, philo->philosophers_id, "has taken a fork");
+		action_print(philo->info, philo->philosophers_id, "has taken a fork");
 	}
 	else
 	{
 		pthread_mutex_lock(&philo->right_fork->fork);
-		action_print(rules, philo->philosophers_id, "has taken a fork");
+		action_print(philo->info, philo->philosophers_id, "has taken a fork");
 
 		pthread_mutex_lock(&philo->left_fork->fork);
-		action_print(rules, philo->philosophers_id, "has taken a fork");
+		action_print(philo->info, philo->philosophers_id, "has taken a fork");
 	}
-	pthread_mutex_lock(&rules->meal_check);
-	philo->last_meal = get_current_time_ms();
-	pthread_mutex_unlock(&rules->meal_check);
+}
 
-	action_print(rules, philo->philosophers_id, "is eating");
-	philo_usleep((long long)rules->time_to_eat * 1000, philo);
-
-	pthread_mutex_lock(&rules->meal_check);
-	philo->meals_amount++;
-	pthread_mutex_unlock(&rules->meal_check);
-
+void unlock_forks(t_philosophers *philo)
+{
 	if (philo->left_fork->id < philo->right_fork->id)
 	{
 		pthread_mutex_unlock(&philo->right_fork->fork);
@@ -119,66 +117,104 @@ void philo_eats(t_philosophers *philo)
 	}
 }
 
-void *philo_routine(void *arg)
+void update_last_meal(t_philosophers *philo)
 {
-	t_philosophers *philo = (t_philosophers *)arg;
-	t_info         *rules = philo->info;
+	pthread_mutex_lock(&philo->info->meal_check);
+	philo->last_meal = get_current_time_ms();
+	pthread_mutex_unlock(&philo->info->meal_check);
+}
 
-	if (rules->philosophers_number == 1)
-	{
-		pthread_mutex_lock(&philo->left_fork->fork);
-		action_print(rules, philo->philosophers_id, "has taken a fork");
+void increment_meals(t_philosophers *philo)
+{
+	pthread_mutex_lock(&philo->info->meal_check);
+	philo->meals_amount++;
+	pthread_mutex_unlock(&philo->info->meal_check);
+}
 
-		long long start = get_current_time_ms();
-		while (1)
-		{
-			pthread_mutex_lock(&rules->dead_lock);
-			int local_finished = rules->finished;
-			pthread_mutex_unlock(&rules->dead_lock);
-			if (local_finished)
-				break;
+void philo_eats(t_philosophers *philo)
+{
+	lock_forks(philo);
 
-			if ((get_current_time_ms() - start) >= rules->time_to_die)
-				break;
-			usleep(50);
-		}
-		pthread_mutex_unlock(&philo->left_fork->fork);
-		return NULL;
-	}
-	if (philo->philosophers_id % 2 == 0)
-		usleep(500);
+	update_last_meal(philo);
+
+	action_print(philo->info, philo->philosophers_id, "is eating");
+	philo_usleep((long long)philo->info->time_to_eat * 1000, philo);
+
+	increment_meals(philo);
+
+	unlock_forks(philo);
+}
+
+void handle_single_philosopher(t_philosophers *philo)
+{
+	t_info *rules = philo->info;
+
+	pthread_mutex_lock(&philo->left_fork->fork);
+	action_print(rules, philo->philosophers_id, "has taken a fork");
+
+	long long start = get_current_time_ms();
 	while (1)
 	{
 		pthread_mutex_lock(&rules->dead_lock);
 		int local_finished = rules->finished;
 		pthread_mutex_unlock(&rules->dead_lock);
 
-		if (local_finished)
+		if (local_finished || (get_current_time_ms() - start) >= rules->time_to_die)
 			break;
 
-		// 1) Eat
+		usleep(50);
+	}
+	pthread_mutex_unlock(&philo->left_fork->fork);
+}
+
+void stagger_start(t_philosophers *philo)
+{
+	if (philo->philosophers_id % 2 == 0)
+		usleep(500);
+}
+
+int check_if_finished(t_info *rules)
+{
+	pthread_mutex_lock(&rules->dead_lock);
+	int local_finished = rules->finished;
+	pthread_mutex_unlock(&rules->dead_lock);
+	return local_finished;
+}
+
+void perform_actions(t_philosophers *philo)
+{
+	t_info *rules;
+
+	rules = philo->info;
+	while (1)
+	{
+		if (check_if_finished(rules))
+			break;
 		philo_eats(philo);
-
-		// 2) Check if finished again
-		pthread_mutex_lock(&rules->dead_lock);
-		local_finished = rules->finished;
-		pthread_mutex_unlock(&rules->dead_lock);
-		if (local_finished)
+		if (check_if_finished(rules))
 			break;
-
-		// 3) Sleep
 		action_print(rules, philo->philosophers_id, "is sleeping");
 		philo_usleep((long long)rules->time_to_sleep * 1000, philo);
 
-		// 4) Check again
-		pthread_mutex_lock(&rules->dead_lock);
-		local_finished = rules->finished;
-		pthread_mutex_unlock(&rules->dead_lock);
-		if (local_finished)
+		if (check_if_finished(rules))
 			break;
-
-		// 5) Think
 		action_print(rules, philo->philosophers_id, "is thinking");
 	}
+}
+
+void *philo_routine(void *arg)
+{
+	t_philosophers *philo = (t_philosophers *)arg;
+	t_info *rules = philo->info;
+
+	if (rules->philosophers_number == 1)
+	{
+		handle_single_philosopher(philo);
+		return NULL;
+	}
+
+	stagger_start(philo);
+	perform_actions(philo);
+
 	return NULL;
 }
